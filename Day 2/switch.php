3355149a8211abac6f11a4de70d58ca50f42b251
Ayush-1211 <63777803+Ayush-1211@users.noreply.github.com>
2021-05-26 12:16:53 +0530

    Day 2 Tasks

---
+++ Day 2/switch.php
@@ -0,0 +1,18 @@
+<?php
+
+    $a = 2;
+    switch($a) {
+        case 1:
+            echo "Case 1";
+            break;
+        case 2:
+            echo "Case 2";
+            break;
+        case 3:
+            echo "Case 3";
+            break;
+        default: 
+            echo "This is default case";
+    }
+
+?>
---
+++ Day 2/variables.php
@@ -0,0 +1,26 @@
+<?php
+
+    $a = 10;
+    $b = 20;
+
+    //Method 1
+    echo "<h1>Method 1</h1>";
+    echo "A value is ".$a;
+    echo "</br>B value is ".$b;
+
+    //Method 2
+    echo "<h1>Method 2</h1>";
+    echo "</br>A value is ".$a."</br>B value is ".$b;
+
+    //Method 3
+    echo "<h1>Method 3</h1>";
+    echo "</br>A value is $a </br>B value is $b";
+
+    //Method 4
+    echo "<h1>Method 4</h1>";
+    echo '</br>A value is $a </br>B value is $b';
+
+    //Method 5
+    echo "<h1>Method 5</h1>";
+    echo "</br>A value is $a </br>B value is $b </br>Sum is ".($a+$b);
+?>
