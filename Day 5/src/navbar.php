47cda0ec6297b39979503bea1f6e56c0dd126ea0
Ayush-1211 <63777803+Ayush-1211@users.noreply.github.com>
2021-05-29 10:38:32 +0530

    Day 5 Tasks

---
+++ Day 5/src/navbar.php
@@ -0,0 +1,9 @@
+<div id="menu-wrapper">
+	<div id="menu">
+		<ul>
+			<li class="current_page_item"><a href="http://localhost/AkashTechnolabs/Day%205/index.php">Homepage</a></li>
+			<li><a href="http://localhost/AkashTechnolabs/Day%205/photos.php">Photos</a></li>
+			<li><a href="http://localhost/AkashTechnolabs/Day%205/contact.php">Contact</a></li>
+		</ul>
+	</div>
+</div>
