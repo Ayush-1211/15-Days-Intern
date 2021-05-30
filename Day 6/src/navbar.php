8a86e25d18eda2d23765eef959ae2659e21dddf6
Ayush-1211 <63777803+Ayush-1211@users.noreply.github.com>
2021-05-30 08:16:55 +0530

    Day 6 Tasks

---
+++ Day 6/src/navbar.php
@@ -0,0 +1,10 @@
+<div id="menu-wrapper">
+	<div id="menu">
+		<ul>
+			<li class="current_page_item"><a href="http://localhost/AkashTechnolabs/Day%206/index.php">Homepage</a></li>
+			<li><a href="http://localhost/AkashTechnolabs/Day%206/photos.php">Photos</a></li>
+			<li><a href="http://localhost/AkashTechnolabs/Day%206/purchase.php">Purchase</a></li>
+			<li><a href="http://localhost/AkashTechnolabs/Day%206/contact.php">Contact</a></li>
+		</ul>
+	</div>
+</div>
