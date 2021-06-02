2e65e058b7a0829ca63a0d985c2fd9ae25484c18
Ayush-1211 <63777803+Ayush-1211@users.noreply.github.com>
2021-06-02 13:19:17 +0530

    Day 8 Tasks

--- Day 8/registration.php
+++ Day 8/registration.php
@@ -34,22 +34,22 @@
                     <form action="reg_action.php" method="post">
 
                         <label>First Name</label>
-                        <input type="text" id="firstname" name="firstname" placeholder="Enter your First name">
+                        <input type="text" id="firstname" name="firstname" placeholder="Enter your First name" required>
                         </br></br>
                         <label>Last Name</label>
-                        <input type="text" id="lastname" name="lastname" placeholder="Enter your Last name">
+                        <input type="text" id="lastname" name="lastname" placeholder="Enter your Last name" required>
                         </br></br>
                         <label>Address</label>
-                        <textarea id="address" name="address" placeholder="Write your address here...." style="height:200px"></textarea>
+                        <textarea id="address" name="address" placeholder="Write your address here...." style="height:200px" required></textarea>
                         </br></br>
                         <label>Email</label>
-                        <input id="email" name="email" placeholder="Write your email here...."></input>
+                        <input id="email" name="email" placeholder="Write your email here...." required></input>
                         </br></br>
                         <label>Username</label>
-                        <input type="text" id="username" name="username" placeholder="Enter your username">
+                        <input type="text" id="username" name="username" placeholder="Enter your username" required>
                         </br></br>
                         <label>Password</label>
-                        <input type="password" id="password" name="password" placeholder="Enter your password">
+                        <input type="password" id="password" name="password" placeholder="Enter your password" required>
                         </br></br>
                         <input type="submit" value="submit" name="submit">
 
